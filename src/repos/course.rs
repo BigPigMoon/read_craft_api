dd5b7e7fc2e0e1c0211e7828b0baa1a081bda737
bigpigmooon <matvey.novikov02@gmail.com>
2023-12-26 18:54:38 +0700

    feat: all method are private

--- src/repos/course.rs
+++ src/repos/course.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, post, web, HttpResponse, Responder};
 
+use crate::extractors::jwt_cred::JwtCred;
+
 pub fn course_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/course")
@@ -13,31 +15,31 @@ pub fn course_config(cfg: &mut web::ServiceConfig) {
 }
 
 #[post("/")]
-pub async fn create_course() -> impl Responder {
+pub async fn create_course(creds: JwtCred) -> impl Responder {
     HttpResponse::NotImplemented()
 }
 
 #[get("/")]
-pub async fn get_courses() -> impl Responder {
+pub async fn get_courses(creds: JwtCred) -> impl Responder {
     HttpResponse::NotImplemented()
 }
 
 #[get("/{id}")]
-pub async fn get_course() -> impl Responder {
+pub async fn get_course(creds: JwtCred) -> impl Responder {
     HttpResponse::NotImplemented()
 }
 
 #[get("/subscribe")]
-pub async fn get_subs() -> impl Responder {
+pub async fn get_subs(creds: JwtCred) -> impl Responder {
     HttpResponse::NotImplemented()
 }
 
 #[post("/subscribe/{id}")]
-pub async fn subscribe() -> impl Responder {
+pub async fn subscribe(creds: JwtCred) -> impl Responder {
     HttpResponse::NotImplemented()
 }
 
 #[get("/owner/{id}")]
-pub async fn is_owner() -> impl Responder {
+pub async fn is_owner(creds: JwtCred) -> impl Responder {
     HttpResponse::NotImplemented()
 }
