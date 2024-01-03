b26cc8ad7d30318920aac20542b39c97c262cbe2
bigpigmooon <matvey.novikov02@gmail.com>
2024-01-03 15:06:37 +0700

    fix: create type if not exists

--- migrations/20231222053953_add_language_enum.up.sql
+++ migrations/20231222053953_add_language_enum.up.sql
@@ -1,28 +1,33 @@
 -- Add up migration script here
-CREATE TYPE language AS ENUM (
-   'bg',
-   'cs',
-   'da',
-   'de',
-   'el',
-   'en',
-   'es',
-   'et',
-   'fi',
-   'fr',
-   'hu',
-   'it',
-   'ja',
-   'lt',
-   'lv',
-   'mt',
-   'nl',
-   'pl',
-   'pt',
-   'ro',
-   'ru',
-   'sk',
-   'sl',
-   'sv',
-   'zh'
-);
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'language') THEN
+    CREATE TYPE language AS ENUM (
+       'bg',
+       'cs',
+       'da',
+       'de',
+       'el',
+       'en',
+       'es',
+       'et',
+       'fi',
+       'fr',
+       'hu',
+       'it',
+       'ja',
+       'lt',
+       'lv',
+       'mt',
+       'nl',
+       'pl',
+       'pt',
+       'ro',
+       'ru',
+       'sk',
+       'sl',
+       'sv',
+       'zh'
+    );
+  END IF;
+END $$;
