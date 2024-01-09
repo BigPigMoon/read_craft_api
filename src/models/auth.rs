8ad2e0cbd3474388ba39dbbe173a6ef592c4db6d
bigpigmooon <matvey.novikov02@gmail.com>
2024-01-09 12:16:25 +0700

    docs: add documentation to auth models

--- src/models/auth.rs
+++ src/models/auth.rs
@@ -1,12 +1,16 @@
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
+/// JWT tokens model
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Tokens {
     pub access: String,
     pub refresh: String,
 }
 
+/// JSON scheme for sign in
+///
+/// Email validating and password min length equal 1
 #[derive(Serialize, Deserialize, Validate, Debug)]
 pub struct SignInData {
     #[validate(email)]
@@ -15,6 +19,11 @@ pub struct SignInData {
     pub password: String,
 }
 
+/// JSON scheme for sign up
+///
+/// Email validating
+///
+/// Password and username min length equal 6
 #[derive(Clone, Serialize, Deserialize, Validate, Debug)]
 pub struct SignUpData {
     #[validate(email)]
