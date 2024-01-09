ce99cf2bacfe5ef9bc1c8cf31de9bccf60554cdf
bigpigmooon <matvey.novikov02@gmail.com>
2024-01-09 12:16:42 +0700

    feat: add course output model

--- src/models/course.rs
+++ src/models/course.rs
@@ -1,6 +1,10 @@
+use crate::services::course::user_is_owner;
+
 use super::language::Language;
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
+use sqlx::{Pool, Postgres};
+use std::error::Error;
 use validator::Validate;
 
 #[derive(Clone, Debug, sqlx::FromRow, Deserialize, Serialize, Validate)]
@@ -11,6 +15,35 @@ pub struct Course {
     #[validate(length(min = 1))]
     pub title: String,
     pub language: Language,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, Validate)]
+pub struct CourseOut {
+    pub id: i32,
+    #[validate(length(min = 1))]
+    pub title: String,
+    pub language: Language,
+    #[serde(rename(serialize = "isOwner", deserialize = "isOwner"))]
+    pub is_owner: bool,
+}
+
+impl CourseOut {
+    pub async fn from_course(
+        course: Course,
+        user_id: i32,
+        pool: &Pool<Postgres>,
+    ) -> Result<CourseOut, Box<dyn Error>> {
+        let is_owner = user_is_owner(user_id, course.id, pool)
+            .await
+            .unwrap_or(false);
+
+        Ok(CourseOut {
+            id: course.id,
+            title: course.title.clone(),
+            language: course.language,
+            is_owner,
+        })
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize, Validate)]
